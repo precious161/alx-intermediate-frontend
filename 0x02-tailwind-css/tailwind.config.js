8197d10d8c8962a6bdd553e266b82a32d773b61a
feyruza dawud <feyruuz1619@gmail.com>
2025-07-07 22:22:26 +0300

    Buit a simple navigation bar using basic flexbox

--- 0x02-tailwind-css/tailwind.config.js
+++ 0x02-tailwind-css/tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./1-index.html", "./2-index.html"],
+  content: [
+    "./src/**/*.{html,js}",
+    "./1-index.html",
+    "./2-index.html",
+    "./3-nav_index.html",
+  ],
   theme: {
     extend: {},
   },
