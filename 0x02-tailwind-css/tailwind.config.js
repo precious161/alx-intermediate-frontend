bf283a060e7ce3aa45311862aafab166784d1f10
feyruza dawud <feyruuz1619@gmail.com>
2025-07-07 18:52:36 +0300

    Added complex grid layouts

--- 0x02-tailwind-css/tailwind.config.js
+++ 0x02-tailwind-css/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./1-index.html"],
+  content: ["./src/**/*.{html,js}", "./1-index.html", "./2-index.html"],
   theme: {
     extend: {},
   },
