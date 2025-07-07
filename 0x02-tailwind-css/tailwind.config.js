21b4bf43a2ca7a37a22bbbe44359cfada42826d6
feyruza dawud <feyruuz1619@gmail.com>
2025-07-07 18:33:02 +0300

    Added some css styling

--- 0x02-tailwind-css/tailwind.config.js
+++ 0x02-tailwind-css/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{html,js}"],
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./1-index.html"],
   theme: {
     extend: {},
   },
